f2e600cc6c8c3328502e4e4a2e2fa10dd4b1aabe
Vivekchaudhari-1012 <118785355+Vivekchaudhari-1012@users.noreply.github.com>
2024-01-22 15:30:28 +0530

    Add files via upload

---
+++ task5/task5.js
@@ -0,0 +1,30 @@
+var inputval = document.querySelector('#cityinput');
+var btn = document.querySelector('#add');
+var city = document.querySelector('#cityoutput');
+var description = document.querySelector('#description');
+var temp = document.querySelector('#temp');
+var wind = document.querySelector('#wind');
+
+var apik = "3045dd712ffe6e702e3245525ac7fa38";
+
+
+function convertion(val) {
+    return (val - 273).toFixed(2);
+}
+
+btn.addEventListener('click', function () {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputval.value + '&appid=' + apik)
+        .then(res => res.json())
+        .then(data => {
+            var nameval = data['name'];
+            var descrip = data['weather'][0]['description']; 
+            var temperature = data['main']['temp'];
+            var windSpeed = data['wind']['speed'];
+
+            city.innerHTML = `City: ${nameval}`;
+            temp.innerHTML = `Temperature: ${convertion(temperature)} C`;
+            description.innerHTML = `Conditions: ${descrip}`;
+            wind.innerHTML = `Wind Speed: ${windSpeed} km/h`;
+        })
+        .catch(err => alert('You entered the wrong city name'));
+});
